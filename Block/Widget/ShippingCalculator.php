50c960fdb9e9affaa0840432f25539953667d57f
hieu01412 <hieu01412@gmail.com>
2020-03-12 12:54:04 +0700

    push code

---
+++ Block/Widget/ShippingCalculator.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Index
+ *
+ * @copyright Copyright © 2020 Landofcoder. All rights reserved.
+ * @author    landofcoder@gmail.com
+ */
+namespace Lof\ShippingCalculator\Block\Widget;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class ShippingCalculator extends Template implements BlockInterface
+{
+    protected $_template="widget/shippingcalculator.phtml";
+
+    public function defaultAddress()
+    {
+        echo getData('title_shipping');
+        die();
+    }
+}
